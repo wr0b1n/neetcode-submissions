381852fb3fdb4e0d288ba4105a4fddc9b3cc0c6c
Robin Lutter <69972430+wr0b1n@users.noreply.github.com>
2026-06-28 13:49:19 +0200

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        # base case, string must have an even number of characters
+        if len(s) % 2 != 0:
+            return False
+
+        dic = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{" 
+        }
+        stack = []
+
+        for c in s:
+            # opening brackets are pushed to the stack
+            if c in dic.values():
+                stack.append(c)             
+
+            # closing brackets must match the top element in the stack
+            elif c in dic.keys():
+                if len(stack) <= 0:
+                    return False
+
+                elem = stack.pop()
+                if elem != dic[c]:
+                    return False
+        
+        return len(stack) == 0
